2d3ed82985e2e0656c65952560b07f6866a13719
felpudodowrico <felpudodowrico@gmail.com>
2026-02-11 21:32:26 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 # config.py
 
 # Configuración del scanner
-MIN_LIQUIDITY = 0         # Liquidez mínima por mercado
-MIN_VOLUME = 0          # Volumen mínimo por mercado
+MIN_LIQUIDITY = 10         # Liquidez mínima por mercado
+MIN_VOLUME = 10          # Volumen mínimo por mercado
 CATEGORIES = None          # Lista de categorías a filtrar, None para todas
 MULTI_OUTCOME = False      # Mostrar solo mercados con más de 2 outcomes
 DISPLAY_DELAY = 0.5         # Segundos entre mostrar mercados
